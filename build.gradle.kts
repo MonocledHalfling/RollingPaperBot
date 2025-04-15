b69b455e7d9b7b9cf61fe75cdf7468df06b5eae3
MonocledHalfling <86958534+MonocledHalfling@users.noreply.github.com>
2025-04-15 20:02:06 +0900

    feat: build patch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    application
 }
 
 group = "org.example"
@@ -10,13 +11,23 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation("net.dv8tion:JDA:5.2.2")
     implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation(kotlin("stdlib"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+application {
+    mainClass.set("org.example.MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.example.MainKt"
+    }
+
+    // JDA 같은 라이브러리 포함된 fat jar 만들기 (Shadow 없이)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
 kotlin {
