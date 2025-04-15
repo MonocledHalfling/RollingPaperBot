7f3d302c4dc10a45f0de4ee12aa482cf0173a30b
MonocledHalfling <86958534+MonocledHalfling@users.noreply.github.com>
2025-04-15 21:53:57 +0900

    feat: change structure

--- src/main/kotlin/data/ConfigLoader.kt
+++ src/main/kotlin/data/ConfigLoader.kt
@@ -8,7 +8,11 @@ import java.io.File
 object ConfigLoader {
     private val mapper = jacksonObjectMapper()
 
-    fun load(path: String = "src/main/resources/config.json"): Config {
-        return mapper.readValue(File(path))
+    fun load(path: String = "config.json"): Config {
+        val file = File(path)
+        if (!file.exists()) {
+            error("❌ 설정 파일(config.json)을 찾을 수 없습니다: $path")
+        }
+        return mapper.readValue(file)
     }
 }
